900f684ad812c0989f6f78d9a9e2b80ab18bcc04
yahsaki <github@yahsaki.com>
2026-09-24 23:02:55 -0700

    implemented all task dal functions

--- server/dal.js
+++ server/dal.js
@@ -56,6 +56,7 @@ class Dal {
       // hmmm should we set the id here or handler? leaving it in handler for now
       console.log(`dal.saveProject: added new project '${project.name}'(${project.id})`)
     } else {
+      // 260924: what was I thinking when I created this else case. truly pointless
       existingProject = project
     }
     const date = new Date()
@@ -106,16 +107,147 @@ class Dal {
   //deleteStory(id, projectId) { throw 'unimplemented' }
   //updateStory(id, projectId, fields) { throw 'unimplemented' }
   // I feel like skipping stories and going straight to tasks
-  getTask(id, projectId) { throw 'unimplemented' }
-  createTask(projectId, task) { throw 'unimplemented' }
-  deleteTask(id, projectId) { throw 'unimplemented' }
-  updateTask(id, projectId, fields) { throw 'unimplemented' }
+  getTask(id, projectId) {
+    const project = this.#data.projects.find(x => x.id === projectId)
+    if (!project) { return }
+    const task = project.tasks.find(x => x.id === id)
+    return task // consider task.data
+  }
+  createTask(projectId, task) {
+    const project = this.#data.projects.find(x => x.id === projectId)
+    // im really trying to push all validation out of this class
+    if (!project) { return {success:false} }
+    project.tasks.push(task)
+
+    this.#data.date_updated = new Date().toISOString()
+
+    util.fs.writeJson(
+      path.join(__dirname, this.#dataPath, this.#dataFilename),
+      this.#data, this.#formatJsonForDebug)
+    console.log(`dal.createTask: data saved to disk`)
+    this.#dirty = true
+
+    return {success:true}
+  }
+  deleteTask(id, projectId) {
+    const project = this.#data.projects.find(x => x.id === projectId)
+    if (!project) { return }
+    const i = project.tasks.findIndex(x => x.id === id)
+    if (i > -1) {
+      project.tasks.splice(i, 1)
+      console.log(`dal.deleteTask: successfully removed task '${id}' from project '${projectId}'`)
+    }
+
+    const date = new Date()
+    this.#data.date_updated = date.toISOString()
+    project.date_updated = date.toISOString()
+
+    util.fs.writeJson(
+      path.join(__dirname, this.#dataPath, this.#dataFilename),
+      this.#data, this.#formatJsonForDebug)
+    console.log(`dal.deleteTask: data saved to disk`)
+    this.#dirty = true
+
+    return {success:true}
+  }
+  updateTask(id, projectId, fields) {
+    let changes = []
+    const project = this.#data.projects.find(x => x.id === projectId)
+    if (!project) { return {success:false} }
+    const task = project.tasks.find(x => x.id === id)
+    if (!task) { return {success:false} }
+    for (const field in fields) {
+      changes.push(`[${field} previous value: '${task[field]}', new value: '${fields[field]}']`)
+      task[field] = fields[field]
+    }
+
+    if (!changes.length) { return {success:false} }
+
+    const date = new Date()
+    this.#data.date_updated = date.toISOString()
+    project.date_updated = date.toISOString()
+    task.date_updated = date.toISOString()
+
+    util.fs.writeJson(
+      path.join(__dirname, this.#dataPath, this.#dataFilename),
+      this.#data, this.#formatJsonForDebug)
+    console.log(`dal.updateTask: data saved to disk`)
+    this.#dirty = true
+
+    return {success:true}
+  }
   // extremely yuck result of using json as a datastore: have to pass all ids along. I refuse to search all
   // projects for a nested comment with a specific id(and the likes). shouldve just used sqlite but still
   // rather not
-  createTaskComment(id, taskId, projectId, comment) { throw 'unimplemented' }
-  deleteTaskComment(id, taskId, projectId) { throw 'unimplemented' }
-  updateTaskComment(id, taskId, projectId, fields) { throw 'unimplemented' }
+  createTaskComment(taskId, projectId, comment) {
+    const project = this.#data.projects.find(x => x.id === projectId)
+    if (!project) { return {success:false} }
+    const task = project.tasks.find(x => x.id === taskId)
+    if (!task) { return {success:false} }
+
+    task.comments.push(comment)
+    
+    const date = new Date()
+    this.#data.date_updated = date.toISOString()
+    project.date_updated = date.toISOString()
+    task.date_updated = date.toISOString()
+
+    util.fs.writeJson(
+      path.join(__dirname, this.#dataPath, this.#dataFilename),
+      this.#data, this.#formatJsonForDebug)
+    console.log(`dal.createTaskComment: data saved to disk`)
+    this.#dirty = true
+
+    return {success:true}
+  }
+  deleteTaskComment(id, taskId, projectId) {
+    const project = this.#data.projects.find(x => x.id === projectId)
+    if (!project) { return {success:false} }
+    const task = project.tasks.find(x => x.id === taskId)
+    if (!task) { return {success:false} }
+    const i = task.comments.findIndex(x => x.id === id)
+    if (i > -1) {
+      task.comments.splice(i, 1)
+      console.log(`dal.deleteTaskComment: successfully removed comment '${id}' from task '${taskId}' from project '${projectId}'`)
+    }
+
+    const date = new Date()
+    this.#data.date_updated = date.toISOString()
+    project.date_updated = date.toISOString()
+    task.date_updated = date.toISOString()
+
+    util.fs.writeJson(
+      path.join(__dirname, this.#dataPath, this.#dataFilename),
+      this.#data, this.#formatJsonForDebug)
+    console.log(`dal.deleteTask: data saved to disk`)
+    this.#dirty = true
+
+    return {success:true}
+  }
+  updateTaskComment(id, taskId, projectId, fields) {
+    const project = this.#data.projects.find(x => x.id === projectId)
+    if (!project) { return {success:false} }
+    const task = project.tasks.find(x => x.id === taskId)
+    if (!task) { return {success:false} }
+    const comment = task.comments.find(x => x.id === id)
+    for (const field in fields) {
+      changes.push(`[${field} previous value: '${comment[field]}', new value: '${fields[field]}']`)
+      comment[field] = fields[field]
+    }
+
+    const date = new Date()
+    this.#data.date_updated = date.toISOString()
+    project.date_updated = date.toISOString()
+    task.date_updated = date.toISOString()
+
+    util.fs.writeJson(
+      path.join(__dirname, this.#dataPath, this.#dataFilename),
+      this.#data, this.#formatJsonForDebug)
+    console.log(`dal.updateTaskComment: data saved to disk`)
+    this.#dirty = true
+
+    return {success:true}
+  }
 }
 
 module.exports = Dal
